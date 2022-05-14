ccc291f0f172e2175b4b7e89bcb6af80ca837c5f
Shubhamrazz <sr99845@gmail.com>
2022-05-14 15:35:03 +0530

    [Shubham]Refactor all 3 to One Generic Method

--- MaximumNumberCheck.cs
+++ MaximumNumberCheck.cs
@@ -8,28 +8,66 @@ namespace FindMaximum_Generics
 {
     class MaximumNumberCheck
     {
-        //Function To check Maximum Integer Number
-        public int MaximumIngerNumber(int firstValue, int secondValue, int thirdValue)
+        /* //Function To check Maximum Integer Number
+         public int MaximumIngerNumber(int firstValue, int secondValue, int thirdValue)
+         {
+             //Comaring 3 Numbers
+             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+             {
+                 return firstValue;
+             }
+             if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+             {
+                 return secondValue;
+             }
+             if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+             {
+                 return thirdValue;
+             }
+             throw new Exception("First, Second and Third number are same : ");
+         }
+         //Function To check Maximum Number
+         public float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
+         {
+             //Comaring 3 Numbers
+             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+             {
+                 return firstValue;
+             }
+             else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+             {
+                 return secondValue;
+             }
+             else if(thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+             {
+                 return thirdValue;
+             }
+             else
+             throw new Exception("First, Second and Third number are same : ");
+         }
+
+         //Function To check Maximum String Number
+         public string MaximumStringNumber(string firstValue, string secondValue, string thirdValue)
+         {
+             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+             {
+                 return firstValue;
+             }
+             if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+             {
+                 return secondValue;
+             }
+             if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+             {
+                 return thirdValue;
+             }
+             throw new Exception("First, Second and Third number are same : ");
+         }
+        */
+
+        //Function To check Maximum Number Using Generics Method
+        public T MaximumNumber<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
         {
-            //Comaring 3 Numbers
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
-            {
-                return thirdValue;
-            }
-            throw new Exception("First, Second and Third number are same : ");
-        }
-        //Function To check Maximum Number
-        public float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
-        {
-            //Comaring 3 Numbers
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
             {
                 return firstValue;
@@ -38,29 +76,11 @@ namespace FindMaximum_Generics
             {
                 return secondValue;
             }
-            else if(thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
             {
                 return thirdValue;
             }
             else
-            throw new Exception("First, Second and Third number are same : ");
-        }
-
-        //Function To check Maximum String Number
-        public string MaximumStringNumber(string firstValue, string secondValue, string thirdValue)
-        {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
-            {
-                return thirdValue;
-            }
             throw new Exception("First, Second and Third number are same : ");
         }
     }
--- Program.cs
+++ Program.cs
@@ -5,26 +5,52 @@ namespace FindMaximum_Generics
    class Program
    {
       static void Main(string[] args)
-      { 
-          //Welcome Message
-          Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
+      {
+            //Welcome Message
+            Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
 
-          //Calling Class to call a Function
-          MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            //Calling Class to call a Function
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
 
-          //Maximum Number At First Place
-          string outputString1 = maximumNumberCheck.MaximumStringNumber("Peach", "Apple", "Banana");
-          Console.WriteLine("Maximum Number of String at First Place : " + outputString1);
+            //Maximum Number At First, Second and Third Place 
+            int outputInt1 = maximumNumberCheck.MaximumNumber<int>(434, 57, 12);
+            int outputInt2 = maximumNumberCheck.MaximumNumber<int>(344, 734, 712);
+            int outputInt3 = maximumNumberCheck.MaximumNumber<int>(343, 344, 1200);
 
-         //Maximum Number At Second Place
-         string outputString2 = maximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
-         Console.WriteLine("Maximum Number of String at Seond Place : " + outputString2);
+            //Maximum Number At First, Second and Third Place 
+            float outputFloat1 = maximumNumberCheck.MaximumNumber<float>(128.9f, 8.9f, 11.75f);
+            float outputFloat2 = maximumNumberCheck.MaximumNumber<float>(12.9f, 48.9f, 28.29f);
+            float outputFloat3 = maximumNumberCheck.MaximumNumber<float>(1.9f, 4.0f, 4.9f);
 
-         //Maximum Number At Third Place
-         string outputString3 = maximumNumberCheck.MaximumStringNumber("Apple", "Banana", "Peach");
-         Console.WriteLine("Maximum Number of String at Third Place : " + outputString3);
+            //Maximum Number At First, Second and Third Place 
+            string outputString1 = maximumNumberCheck.MaximumNumber<string>("Peach", "Apple", "Banana");
+            string outputString2 = maximumNumberCheck.MaximumNumber<string>("Apple", "Peach", "Banana");
+            string outputString3 = maximumNumberCheck.MaximumNumber<string>("Apple", "Banana", "Peach");
 
-         Console.ReadLine();
+            //Display Results
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Results Of an Integer : ");
+            Console.WriteLine("Integer at First Place : " + outputInt1);
+            Console.WriteLine("Integer at Second Place : " + outputInt2);
+            Console.WriteLine("Integer at Third Place : " + outputInt3);
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Results Of an Float : ");
+            Console.WriteLine("Float at First Place : " + outputFloat1);
+            Console.WriteLine("Float at Second Place : " + outputFloat2);
+            Console.WriteLine("Float at Third Place : " + outputFloat3);
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Results Of an String : ");
+            Console.WriteLine("String at First Place : " + outputString1);
+            Console.WriteLine("String at Second Place : " + outputString2);
+            Console.WriteLine("String at Third Place : " + outputString3);
+            Console.WriteLine("----------------------------------------");
+
+            Console.ReadLine();
       }
-    }
+   }
 }
+  
