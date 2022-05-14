d755401be04a46c327205299332d631719658c05
Shubhamrazz <sr99845@gmail.com>
2022-05-14 07:07:28 +0530

    [Shubham]Tc 1.1-Max Number At First Place

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindMaximum_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome to Maximum Number");
+
+            //Calling Class to call a Function
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+
+            //Maximum Number At First Place
+            int outputInteger = maximumNumberCheck.MaximumIngerNumber(411, 282, 333);
+
+            //Display Output
+            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+
+            Console.ReadLine();
+
+        }
+    }
+}
