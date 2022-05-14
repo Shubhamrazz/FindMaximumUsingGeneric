6af876901a360a72748b2360f0e850398160d3ed
Shubhamrazz <sr99845@gmail.com>
2022-05-14 13:47:36 +0530

    [Shubham]Given 3 Float values to find the maximum

--- MaximumNumberCheck.cs
+++ MaximumNumberCheck.cs
@@ -8,8 +8,26 @@ namespace FindMaximum_Generics
 {
     class MaximumNumberCheck
     {
-        //Function To check Maximum Number
+        //Function To check Maximum Integer Number
         public int MaximumIngerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            //Comaring 3 Numbers
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("First, Second and Third number are same : ");
+        }
+        //Function To check Maximum Number
+        public float MaximumFloatNumber(float firstValue, float secondValue, float thirdValue)
         {
             //Comaring 3 Numbers
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
--- Program.cs
+++ Program.cs
@@ -2,24 +2,29 @@
 
 namespace FindMaximum_Generics
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Welcome Message
-            Console.WriteLine("Welcome to Maximum Number");
+   class Program
+   {
+      static void Main(string[] args)
+      { 
+         //Welcome Message
+         Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
 
-            //Calling Class to call a Function
-            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+        //Calling Class to call a Function
+        MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
 
-            //Find Maximum Integer
-            int outputInteger = maximumNumberCheck.MaximumIngerNumber(11,111,1111);
+        //Maximum Number At First Place
+        float outputFloat1 = maximumNumberCheck.MaximumFloatNumber(14.1f, 2.82f, 7.33f);
+        Console.WriteLine("Maximum Number of Float at First Place : " + outputFloat1);
 
-            //Display Output
-            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+        //Maximum Number At Second Place
+        float outputFloat2 = maximumNumberCheck.MaximumFloatNumber(4.1f, 28.2f, 12.8f);
+        Console.WriteLine("Maximum Number of Float at Second Place : " + outputFloat2);
 
-            Console.ReadLine();
+        //Maximum Number At Third Place
+        float outputFloat3 = maximumNumberCheck.MaximumFloatNumber(75.9f, 68.47f, 77.3f);
+        Console.WriteLine("Maximum Number of Float at Third Place : " + outputFloat3);
 
-        }
+         Console.ReadLine();
+      }
     }
 }
