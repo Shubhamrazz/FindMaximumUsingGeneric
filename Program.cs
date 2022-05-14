ea451ceb2cd12d22f7533990ff197b35fb846bdd
Shubhamrazz <sr99845@gmail.com>
2022-05-14 14:05:14 +0530

    [Shubham]Given 3 string values find maximum

--- Program.cs
+++ Program.cs
@@ -6,23 +6,23 @@ namespace FindMaximum_Generics
    {
       static void Main(string[] args)
       { 
-         //Welcome Message
-         Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
+          //Welcome Message
+          Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
 
-        //Calling Class to call a Function
-        MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+          //Calling Class to call a Function
+          MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
 
-        //Maximum Number At First Place
-        float outputFloat1 = maximumNumberCheck.MaximumFloatNumber(14.1f, 2.82f, 7.33f);
-        Console.WriteLine("Maximum Number of Float at First Place : " + outputFloat1);
+          //Maximum Number At First Place
+          string outputString1 = maximumNumberCheck.MaximumStringNumber("Peach", "Apple", "Banana");
+          Console.WriteLine("Maximum Number of String at First Place : " + outputString1);
 
-        //Maximum Number At Second Place
-        float outputFloat2 = maximumNumberCheck.MaximumFloatNumber(4.1f, 28.2f, 12.8f);
-        Console.WriteLine("Maximum Number of Float at Second Place : " + outputFloat2);
+         //Maximum Number At Second Place
+         string outputString2 = maximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
+         Console.WriteLine("Maximum Number of String at Seond Place : " + outputString2);
 
-        //Maximum Number At Third Place
-        float outputFloat3 = maximumNumberCheck.MaximumFloatNumber(75.9f, 68.47f, 77.3f);
-        Console.WriteLine("Maximum Number of Float at Third Place : " + outputFloat3);
+         //Maximum Number At Third Place
+         string outputString3 = maximumNumberCheck.MaximumStringNumber("Apple", "Banana", "Peach");
+         Console.WriteLine("Maximum Number of String at Third Place : " + outputString3);
 
          Console.ReadLine();
       }
